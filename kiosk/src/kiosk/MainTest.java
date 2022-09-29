4ea9f64ca04e9b45fa8ce041d4b61757f440b74a
heeseon0429 <112688192+heeseon0429@users.noreply.github.com>
2022-09-29 20:45:31 +0900

    Add files via upload
    2022.09.29

--- kiosk/src/kiosk/MainTest.java
+++ kiosk/src/kiosk/MainTest.java
@@ -1,77 +1,150 @@
 package kiosk;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class MainTest {
 
-	JFrame f;
-	JButton btn, aBtn, bBtn, cBtn, dBtn;
-	JLabel lab;
+   JFrame f;                  // 프레임
+   JButton bPay, bCancel;       // 결제, 취소 버튼
+   JLabel lab;                  // 라벨
+   JTextArea taMenu;           // 결제창(메뉴) 
+   JTextField taCost;           // 결제창(금액)
+   JLabel total, order;        //
+   
+   // 결제액
+   int sum;
+   
+   PanelA panelA;
+   PanelB panelB;
+   PanelC panelC;
+   PanelD panelD;
+   
+   MainTest(){
+      // * 프레임
+      f = new JFrame("Kiosk 주문창");
+      
+      // 결제와 취소 버튼을 생성
+      bPay = new JButton("결제");
+      bCancel = new JButton("취소");
+      
+      taMenu = new JTextArea("담은 메뉴\n"); // 메뉴 담는 창 생성
+      taCost = new JTextField("결제 금액"); // 담은 메뉴 금액 표시 창 생성
+      
+      // 결제 총액 라벨을 생성
+      total = new JLabel("결제액");
+      
+      // * (상단) 패널
+      panelA = new PanelA(this);
+      panelB = new PanelB(this);
+      panelC = new PanelC(this);
+      panelD = new PanelD(this);
+   
+      
+   } // end of MainTest
+   
+   void addLayout(){
+      
+      f.setLayout(new BorderLayout());
+      
+      // 메뉴탭 만들기
+      JTabbedPane tab = new JTabbedPane();
+      tab.addTab("메인 메뉴", panelA);
+      tab.addTab("사이드 메뉴", panelB);
+      tab.addTab("음료", panelC);
+      tab.addTab("주류", panelD);
+      
+      // 메뉴탭 붙이기
+      f.add(tab, BorderLayout.CENTER);
+      
+      // 주문 메뉴 및 결제 금액 창
+      JPanel order = new JPanel();
+      order.add(taMenu, BorderLayout.EAST);
+      order.add(taCost, BorderLayout.SOUTH);
+      f.add(order, BorderLayout.SOUTH);
+      
+      // 오른쪽 패널 (목적: 주문, 결제)
+      JPanel pSouth = new JPanel(new BorderLayout());
+         pSouth.setPreferredSize(new Dimension(0, 200));
+         
+      JPanel payZone = new JPanel(new GridLayout(2,1));
+         payZone.add(bCancel);
+         payZone.add(bPay);
+         payZone.setPreferredSize(new Dimension(120, 0));
+         pSouth.add(payZone, BorderLayout.EAST);
 
-	Panel panelA;
-	Panel panelB;
-	Panel panelC;
-	Panel panelD;
+      pSouth.add(taMenu, BorderLayout.CENTER);
 
-	MainTest(){
+      JPanel payView = new JPanel(new BorderLayout());
+         payView.add(total, BorderLayout.WEST);
+         payView.add(taCost, BorderLayout.CENTER);
 
-		f = new JFrame("Kiosk 주문창");
-		aBtn = new JButton("떡볶이");
-		bBtn = new JButton("모짜감자");
-		cBtn = new JButton("치즈볼");
-		dBtn = new JButton("파인샤벳");
-		panelA = new Panel();
-		panelB = new Panel();
-		panelC = new Panel();
-		panelD = new Panel();
+      pSouth.add(payView, BorderLayout.SOUTH);
 
-	} // end of MainTest
+      f.add(pSouth, BorderLayout.SOUTH);
+      
+      // 결제창에 결제액이 들어감
+       taCost.setText(String.valueOf(sum) + "원");
 
-	void addLayout(){
 
-		JPanel pCenter = new JPanel(new GridLayout(2,2));
-		//f.setLayout(new GridLayout(2,2));
-		
-		pCenter.add(aBtn);
-		pCenter.add(bBtn);
-		pCenter.add(cBtn);
-		pCenter.add(dBtn);
-		
-		JTabbedPane tab = new JTabbedPane();
-		tab.addTab("메인 메뉴", panelA);
-		tab.addTab("사이드 메뉴", panelB);
-		tab.addTab("음료", panelC);
-		tab.addTab("주류", panelD);
+      f.setBounds(300, 100, 500, 1000);
+      f.setVisible(true);
+      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+   } // end of addLayout()
+   
+   void eventProc() {
+      // 결제 버튼 눌렀을 때 최종 결제금액이 뜨게 팝업 만들기?
+      bPay.addActionListener(new ActionListener() {   
+         public void actionPerformed(ActionEvent e) {
+            
 
-		f.add(tab, BorderLayout.CENTER);
-		
-		f.add(pCenter);
-		f.setBounds(100, 100, 500, 400);
-		f.setVisible(true);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         } // end of actionPerformed      
 
-	} // addLayout()
-	
-	void eventProc() {
+      } ); // end of add.ActionListener()
+      
+      bCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	sum = 0;
+               taMenu.setText("");
+               taCost.setText("");
+            }   // end of ActionListener
+         });   // end of addActionListener
 
-	} // end of eventProc()
+      bPay.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               int reply = JOptionPane.showConfirmDialog(null, "결제하시겠습니까?","결제창", JOptionPane.YES_NO_OPTION);
+               if (reply == JOptionPane.YES_OPTION) {
+            	   sum = 0;
+                  taMenu.setText("");
+                  taCost.setText("");
+                  JOptionPane.showMessageDialog(null, "결제되었습니다.");
+               } else {
+                  JOptionPane.showMessageDialog(null, "취소되었습니다.");
+               }// 결제창 구문 Y/N
+            }   // end of ActionListener
+         });   // end of addActionListener
+      
+   } // end of eventProc()
+   
+   public static void main(String[] args) {
 
-	public static void main(String[] args) {
-
-		MainTest test = new MainTest();
-		test.addLayout();
-		test.eventProc();
-	} // end of main
-	
+      MainTest test = new MainTest();
+      test.addLayout();
+      test.eventProc();
+      
+   } // end of main
+   
 }
-
-
-	
